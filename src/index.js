10b4efd9cce8d0292ca7f39b22d752a6dca6b6e2
Will Sexton <williambsexton@gmail.com>
2020-05-23 17:29:37 -0700

    add title screen

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import GameView from './game_view'
 console.log("Webpack is working!")
 document.addEventListener("DOMContentLoaded", () => {
   let canvas = document.getElementById("game-canvas")
+  let button = document.getElementById("theButton")
   let ctx = canvas.getContext("2d")
   canvas.height = 450;
   canvas.width = 900;
   ctx.fillRect(20,20, 1200, 800)
+  button.addEventListener("click", ()=> {
+    let modal = document.getElementById("outer")
+    modal.className = 'hidden'
+    let inputs = {}
+    document.addEventListener("keydown", (e) => {inputs[e.key] = true}, false);
+    document.addEventListener("keyup", (e) => {inputs[e.key] = false}, false);
+    let game = new Game(inputs)
+    let gameview = new GameView(game, ctx)
+    gameview.start();
+  })
   
-  let inputs = {}
-  document.addEventListener("keydown", (e) => {inputs[e.key] = true}, false);
-  document.addEventListener("keyup", (e) => {inputs[e.key] = false}, false);
-  let game = new Game(inputs)
-  let gameview = new GameView(game, ctx)
-  gameview.start();
 })
